9d1cc5c288a996bff97d670bc71d5d1fdcf444ae
Gianpaolo Pascasi <pascasigianpaolo@gmail.com>
2023-04-23 15:01:23 +0200

    chore: http port in env

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-const PORT = 3000;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
@@ -17,7 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(PORT);
-  Logger.log(`App started on port ${PORT}`, 'Server');
+  await app.listen(process.env.HTTP_PORT ?? 3000);
+  Logger.log(`App started on port ${process.env.HTTP_PORT ?? 3000}`, 'Server');
 }
 bootstrap();
